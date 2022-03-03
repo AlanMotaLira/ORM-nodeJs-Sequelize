12cea7dcdfe41c8a7d720c2edca899a0847f5d54
Alan Lira <alan.lira08@gmail.com>
2022-03-03 17:42:17 -0300

    finalizando o modulo 4

--- api/controllers/PessoasControllers.js
+++ api/controllers/PessoasControllers.js
@@ -25,8 +25,34 @@ class PessoaController {
   static async criarPessoa(req, res) {
     const novaPessoa = req.body;
     try {
-      const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
-      return res.status(200).json(novaPessoaCriada)
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
+
+  static async atualizarRegistro(req, res) {
+    const { id } = req.params;
+    const atualizarPessoa = req.body;
+    try {
+      await database.Pessoas.update(atualizarPessoa, {
+        where: { id: Number(id) },
+      });
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoaAtualizada);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
+
+  static async removerPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({ where: { id: Number(id) } });
+      return res.state(200).json({ mensagem: `O id = ${id} removido` });
     } catch (err) {
       return res.status(500).json(err);
     }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id',PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criarPessoa)
+router.put('/pessoas/:id', PessoaController.atualizarRegistro)
+router.delete('/pessoas/:id', PessoaController.removerPessoa)
 
 module.exports = router
