4d15a8a303ba2a3a804ce1df6f1b972ef22c437d
Alan Lira <alan.lira08@gmail.com>
2022-05-16 22:29:03 -0300

    adicionando validação da criação do usuario, pelo o registro do sequelize

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,33 +1,50 @@
-'use strict'
-const {
-  Model
-} = require('sequelize')
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
-    static associate (models) {
+    static associate(models) {
       Pessoas.hasMany(models.Turmas, {
-        foreignKey: 'docente_id'
-      })
+        foreignKey: "docente_id",
+      });
       Pessoas.hasMany(models.Matriculas, {
-        foreignKey: 'estudante_id'
-      })
+        foreignKey: "estudante_id",
+      });
     }
   }
-  Pessoas.init({
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pessoas',
-    paranoid: true,
-    defaultScope: {
-      where: { ativo: true }
+  Pessoas.init(
+    {
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [2, 50],
+            msg: "Por favor, informe o nome",
+          },
+        },
+      },
+      ativo: DataTypes.BOOLEAN,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "dados de email inválido",
+          },
+        },
+      },
+      role: DataTypes.STRING,
     },
-    scopes: {
-      todos: { where: {} }
+    {
+      sequelize,
+      modelName: "Pessoas",
+      paranoid: true,
+      defaultScope: {
+        where: { ativo: true },
+      },
+      scopes: {
+        todos: { where: {} },
+      },
     }
-  })
-  return Pessoas
-}
+  );
+  return Pessoas;
+};
